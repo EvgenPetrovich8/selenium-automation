eab29a16b9fe1c0439ab696d066d1b7c96b6f27c
EvgenPetrovich8 <evgenpetrovich8@gmail.com>
2020-06-22 21:40:47 +0300

    home tasks PageObjects and Add,Edit,Del Article

--- src/test/java/io/react/realworld/UserRegistrationTestOld.java
+++ src/test/java/io/react/realworld/UserRegistrationTestOld.java
@@ -16,7 +16,7 @@ import java.util.concurrent.TimeUnit;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.testng.Assert.assertEquals;
 
-public class UserRegistrationTest {
+public class UserRegistrationTestOld {
 
     private WebDriver driver;
  String userName = "evgenpetrovich"+new Random().nextInt(5000);
